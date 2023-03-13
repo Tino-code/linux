5cb1fe1c69eb01cb5bb4c7d333aef4de738aad42
Tino-code <martintetteh1350@gmail.com>
2023-03-12 20:30:02 -0400

    making progress

---
+++ atm.cpp
@@ -0,0 +1,78 @@
+#include<iostream>
+using namespace std;
+
+float isDeposits ();
+int isWithdrawal();
+
+
+
+
+
+
+ 
+ int main(){
+ int password =1234;
+ int enterPassword;
+ cout << "Enter screate pin: ";
+ cin>>enterPassword;
+ // Main block.
+if(enterPassword == password){ 
+    cout<<"*----MAIN MENU----*";
+
+
+
+
+
+}
+else
+  
+    for(int i=0; i<=4; i++){
+      
+      int k;
+      i += k;
+
+    
+     if (k < 3){
+ while (enterPassword != password){
+
+    cout << "Enter screate pin: ";
+    cin>>enterPassword;
+    }
+
+     }else
+        cout<<"Your account has been blocked"<< endl;
+        break;
+     
+   }
+
+    
+
+   
+
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+ }
+
+float isDeposits (float amountDeposit){
+
+
+
+
+
+}
